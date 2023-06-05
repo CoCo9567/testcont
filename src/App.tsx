4c3da085ad4bb9ef3399df4ed6c4bf02f42cfc64
余晖 <2350553579@qq.com>
2023-06-05 09:10:21 +0800

    fix(views): 修复导出的组件为空

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '@/views/Home';
-// import Bar from "@/components/Bar";
 // 如何提取其他组件的props类型
 // React.ComponentProps<T>
 // 对于内置的jsx元素的类型 我们直接使用标签名字符串来计算 如div
@@ -10,25 +9,12 @@ import Home from '@/views/Home';
 // const b: React.ComponentProps<"a">
 function App() {
   return (
-    <>
-    {/* <Bar
-      a={1}
-      c={{
-        width:100,
-        height:100,
-        backgroundColor:"red",
-      }}
-      d={[1,2,3]}
-      >
-        <div>124996</div>
-        </Bar> */}
-        <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
